f6f6e72712e302d82b30e3f16cf75bf45420057b
Andrii Stafiniak <andrii.stafiniak@gmail.com>
2024-09-17 14:29:11 +0200

    resolve conflict

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,7 @@ import { Experience } from "./components/Experience";
 
 function App() {
   return (
-<<<<<<< HEAD
     <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-=======
-    <Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }}>
->>>>>>> 9d3c5be (Initial commit)
       <color attach="background" args={["#ececec"]} />
       <Experience />
     </Canvas>
