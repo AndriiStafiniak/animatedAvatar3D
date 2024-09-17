f06ba6b2e816949003a3c96ecd2550d65d50061c
Andrii Stafiniak <andrii.stafiniak@gmail.com>
2024-09-17 09:58:59 +0200

    Initial commit

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { OrbitControls } from "@react-three/drei";
 
 export const Experience = () => {
@@ -8,6 +9,46 @@ export const Experience = () => {
         <boxGeometry />
         <meshNormalMaterial />
       </mesh>
+=======
+import { Environment, OrbitControls, Sky } from "@react-three/drei";
+import { Avatar } from "./Avatar"
+import { useControls } from "leva";
+export const Experience = () => {
+  const { animation } = useControls({
+    animation: {
+      value: "Typing",
+      options: ["Typing", "Talking", "Dancing", "Walking"],
+    },
+  })
+  return (
+    <>
+      <OrbitControls />
+      <Sky />
+      <Environment preset="sunset" />
+      <group position-y={-1}>
+        <Avatar animation={animation} />
+        {
+          animation === "Typing" && (
+            <mesh
+              scale={[0.8, 0.5, 0.8]}
+              position-y={0.25}
+            >
+              <boxGeometry />
+              <meshStandardMaterial color={"teal"} />
+            </mesh>
+          )
+        }
+
+        <mesh
+          receiveShadow
+          scale={5}
+          rotation-x={-Math.PI * 0.5}
+        >
+          <planeGeometry />
+          <meshStandardMaterial color={"white"} />
+        </mesh>
+      </group>
+>>>>>>> 9d3c5be (Initial commit)
     </>
   );
 };
